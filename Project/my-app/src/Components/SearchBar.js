49309eb6143ec950ae60e9af9269c9c80175f061
BuildTools <unconfigured@null.spigotmc.org>
2022-11-08 19:48:09 -0600

    Added the Micheal/Matthew Spotify API integration

---
+++ Project/my-app/src/Components/SearchBar.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
+function SearchBar({ placeholder, data }) {
+
+    const [filteredData, setFilteredData] = useState([]);
+    const [nameEntered, setNameEntered] = useState("");
+
+    const handleFilter = (event) => {
+        const searchName = event.target.value;
+        setNameEntered(searchName);
+        const newFilter = data.filter((value) => {
+            return value.name.toLowerCase().includes(searchName.toLowerCase());
+        });
+
+        if (searchName === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    };
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setNameEntered("");
+    };
+
+    return (
+        <div className="search">
+            <div className="searchInputs">
+                <input 
+                    type="text"
+                    placeholder={placeholder}
+                    value={nameEntered}
+                    onChange={handleFilter}
+                />
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? (
+                    <SearchIcon /> 
+                    ) : (
+                    <CloseIcon id="clearBtn" onClick={clearInput} />
+                    )}
+                </div>
+            </div>
+            {filteredData.length !== 0 && (
+            <div className="dataResult">
+                {filteredData.slice(0, 5).map((value, key) => {
+                    return (
+                        <a className="dataItem" href={value.image_url} target="_blank"> 
+                            <p> {value.name} </p>
+                        </a>
+                    );
+                })}
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default SearchBar
