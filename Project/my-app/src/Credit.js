d6c7b930a9fabe078ab5a9ad6aff2e84e485c952
tiredslytherclaw <m00nlitsouls03@gmail.com>
2022-12-01 15:05:59 -0600

    added a footer with credit (not pretty rn)

---
+++ Project/my-app/src/Credit.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Credit = () => (
+    <div className="Credit">
+      <p>This is some content the credit</p>
+    </div>
+  );
+  
+  export default Credit;
