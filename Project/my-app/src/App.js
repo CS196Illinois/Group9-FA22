d94b5bdeab3c71c90cd5f70f236c76f0a76877fc
tiredslytherclaw <m00nlitsouls03@gmail.com>
2022-12-01 20:13:46 -0600

    added our logo to the top

--- Project/my-app/src/App.js
+++ Project/my-app/src/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 import SearchBar from "./Components/SearchBar";
 import PlaylistData from "./Data.json";
 import Credit from "./Credit.js";
+import Logo from "./Logo";
+import { alignProperty } from "@mui/material/styles/cssUtils";
 
 function App() {
   return (
     <div className="App">
+      <Logo />
       <SearchBar placeholder="Enter an album..." data={PlaylistData} />
       <Credit/>
     </div>
---
+++ Project/my-app/src/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import menken_text from './menken_text.png';
+import "./index.css";
+
+function Logo() {
+    return <img src={menken_text} width={200} height={200} alt="our logo" />;
+}
+
+export default Logo;
